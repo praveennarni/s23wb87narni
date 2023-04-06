0992770bacf6de721c0ce273e760499549df4dcd
praveennarni <praveenbabunarni1999@gmail.com>
2023-04-05 21:46:53 -0500

    Changes done

--- routes/gift.js
+++ routes/gift.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('gift', { title: 'Search Results for Gift' });
+  res.render('gift', { title: 'Search Results' });
 });
 
 module.exports = router;
